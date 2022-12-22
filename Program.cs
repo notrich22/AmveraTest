4a14a9a9201b0e6b08fdcecc3abd42e4e1310be8
notrich22 <notrich22@mail.ru>
2022-12-22 18:36:58 +0100

    Methods updated

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async (context) =>await context.Response.WriteAsJsonAsync($"Server is running. Current date/time: {DateTime.Now}" ));
+app.MapGet("/ping", async (context) =>await context.Response.WriteAsJsonAsync($"Pong: {DateTime.Now}!" ));
 
 app.Run();
